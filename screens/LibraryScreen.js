35af62b6b1b8954df93a55cb9d4926d079d84425
Jetdoh <kantinant5673@gmail.com>
2023-08-17 20:34:24 +0900

    change location of data to a seperated file

--- screens/LibraryScreen.js
+++ screens/LibraryScreen.js
@@ -5,6 +5,7 @@ import { View, Text, StyleSheet, SafeAreaView, TextInput, FlatList } from 'react
 import IconContainer from '../components/IconContainer';
 import { Search } from 'react-native-feather';
 import SearchFilter from '../components/SearchFilter';
+import data from '../data';
 
 const LibraryScreen = () => {
   //input for search bar
@@ -103,36 +104,38 @@ const styles = StyleSheet.create({
 export default LibraryScreen;
 
 
-// example data for SearchFilter
-const data = [
-  {
-    name: 'lasagna',
-    imageSource: require('../assets/pics/lasagna.jpg'),
-    color: '#FFCCDE',
-    description: 'description 1',
-  },
-  {
-    name: 'pizza',
-    imageSource: require('../assets/pics/pizza.jpg'),
-    color: '#BDE8E8',
-    description: 'description 2',
-  },
-  {
-    name: 'sushi',
-    imageSource: require('../assets/pics/sushi.jpg'),
-    color: '#F0D1A5',
-    description: 'description 3',
-  },
-  {
-    name: 'kapao',
-    imageSource: require('../assets/pics/kapao.jpg'),
-    color: '#FFCCDE',
-    description: 'description 4',
-  },
-  {
-    name: 'somtum',
-    imageSource: require('../assets/pics/somtum.jpg'),
-    color: '#FFCCDE',
-    description: 'description 5',
-  },
-];
+// delete this later
+
+// // example data for SearchFilter
+// const data = [
+//   {
+//     name: 'lasagna',
+//     imageSource: require('../assets/pics/lasagna.jpg'),
+//     color: '#FFCCDE',
+//     description: 'description 1',
+//   },
+//   {
+//     name: 'pizza',
+//     imageSource: require('../assets/pics/pizza.jpg'),
+//     color: '#BDE8E8',
+//     description: 'description 2',
+//   },
+//   {
+//     name: 'sushi',
+//     imageSource: require('../assets/pics/sushi.jpg'),
+//     color: '#F0D1A5',
+//     description: 'description 3',
+//   },
+//   {
+//     name: 'kapao',
+//     imageSource: require('../assets/pics/kapao.jpg'),
+//     color: '#FFCCDE',
+//     description: 'description 4',
+//   },
+//   {
+//     name: 'somtum',
+//     imageSource: require('../assets/pics/somtum.jpg'),
+//     color: '#FFCCDE',
+//     description: 'description 5',
+//   },
+// ];
